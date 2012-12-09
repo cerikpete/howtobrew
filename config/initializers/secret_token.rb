0eeeca63a13e5efb79cc00d76bd652f0ef1418e5
Erik Peterson <cerikpete@gmail.com>
2012-12-09 13:41:08 -0500

    Initial commit of app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Howtobrew::Application.config.secret_token = '5243ab720e01996b6b0bba4d53160178ed8534f3d5cf8019189513069c596c93fc392a8954e5ed2b7fc9f5aaa9f7bb62ac62b5ab69710d5275d6f08dd24fb860'
