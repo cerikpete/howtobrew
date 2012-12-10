a0be52b2498a892f51667725fd27648b733ddcac
Erik Peterson <cerikpete@gmail.com>
2012-12-09 19:51:13 -0500

    Added equipment view, and added helper to set the title on a page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Howtobrew::Application.routes.draw do
   get "home/index"
   
   root :to => 'home#index'
+  match 'equipment' => 'equipment#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
